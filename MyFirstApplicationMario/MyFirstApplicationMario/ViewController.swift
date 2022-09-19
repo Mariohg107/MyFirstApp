801bd7a698638887a0371aa5d30e3f076fb9d972
Mario <mariohgcte@gmail.com>
2022-09-19 20:32:25 +0200

    init project

---
+++ MyFirstApplicationMario/MyFirstApplicationMario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApplicationMario
+//
+//  Created by Alumnos on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("")
+    }
+
+
+}
+
